a0c6c4b8e6fb0aaaa03024893ad8e2b94f564b14
surenmarahatta <58402263+surenmarahatta@users.noreply.github.com>
2023-12-28 11:43:07 -0500

    Update a1_DataTypesAndSize.java
    no comment

--- LearningJava/src/a_basics_Of_Java/a1_DataTypesAndSize.java
+++ LearningJava/src/a_basics_Of_Java/a1_DataTypesAndSize.java
@@ -13,6 +13,6 @@ public class a1_DataTypesAndSize {
 	 * char  -  2 bytes
 	.............. */
 	
-	//Adding comment to test the synch and fetch
+	//please refer to w3 school for more detail about this
 	
 }
